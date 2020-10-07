70d51ef2e584b364ba61e6041fa781d44e173326
KobeHill68 <kobehill68@gmail.com>
2020-10-07 13:43:00 -0500

    new info

--- questions.js
+++ questions.js
@@ -1,23 +1,25 @@
-var questions = [
+var questionsArray = [
+
     {
-        title : "What does CSS stand for?",
+        question: "What does CSS stand for?",
         choices: ["Color Style Sheet", "Cascading Style Sheets", "Cascading Sheets Style",],
         answer: "Cascading Style Sheets",
     },
     {
-        title : "How do you define variables in javascript?",
+        question: "How do you define variables in javascript?",
         choices: ["var", "VAR", "<var>",],
         answer: "var",
     },
     {
-        title : "Who Invented JavaScript?",
+        question: "Who Invented JavaScript?",
         choices: ["Bill Gates", "Brendan Eirch", "Kobe Hill",],
         answer: "Brendan Eirch",
     },
     {
-        title : "How do you make a For Loop?",
+        question : "How do you make a for Loop?",
         choices: ["For=loop", "for()", "none of the above",],
         answer: "for()",
     },
+   
 ];
 
