d9120ace174ad977657e0cd51e787c1a90f11f83
KobeHill68 <kobehill68@gmail.com>
2020-10-07 22:59:08 -0500

    new java

--- quizlogic.js
+++ quizlogic.js
@@ -1,8 +1,8 @@
-var timeEl = document.getElementById("time");
-var startButton = document.getElementById("startquiz");
-var questionsEl = document.getElementById("questions");
-var oLContainerEl = document.getElementById("ol-container");
-var choicesListOl = document.getElementById("choices-list");
+var timeElement = document.getElementById("time");
+var startButton = document.getElementById("startQuiz");
+var questionsElement = document.getElementById("questions");
+var uLContainerElement = document.getElementById("ul-container");
+var choicesListUl = document.getElementById("choices-list");
 
 var timeLeft = 0;
 var question = 0;
@@ -16,28 +16,31 @@ var questionsArray = [
         choices: ["Color Style Sheet", "Cascading Style Sheets", "Cascading Sheets Style",],
         answer: "Cascading Style Sheets",
     },
+
     {
         question: "How do you define variables in javascript?",
         choices: ["var", "VAR", "<var>",],
         answer: "var",
     },
+
     {
         question: "Who Invented JavaScript?",
         choices: ["Bill Gates", "Brendan Eirch", "Kobe Hill",],
         answer: "Brendan Eirch",
     },
+
     {
-        question : "How do you make a for Loop?",
+        question: "How do you make a for Loop?",
         choices: ["For=loop", "for()", "none of the above",],
         answer: "for()",
     },
-   
+
 ];
 
 
 
 
-function startTimeQuiz() {
+function startQuiz() {
     timeLeft = 60;
     question = 0;
     userScore = 0;
@@ -47,78 +50,79 @@ function startTimeQuiz() {
 
 
 function setTime() {
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft;
+    var timeInterval = setInterval(function () {
+        secondsLeft = 60;
+        timeElement.textContent = secondsLeft;
 
         if (secondsLeft === 0 || questions === questionsArray.length) {
             userScore = secondsLeft;
-            clearInterval(timerInterval);
+            clearInterval(timeInterval);
             quizOver();
         }
-    }, 100);
+    }, 1000);
 };
 
 function theQuestions() {
-    if(questions < questionsArray.length){
-        questionsEl.textContent = questionsArray[questions].questions;
+    if (questions < questionsArray.length) {
+        questionsElement.textContent = questionsArray[questions].questions;
         answer = questionsArray[questions].answer;
         theChoices();
-0   };
+    };
 };
 
 
-function theChoices(){
-    appendOlEl();
 
-    for(var i = 0; i < questionsArray[questions].choices.length; i++){
+
+function theChoices() {
+    appendUlEl();
+
+    for (var i = 0; i < questionsArray[questions].choices.length; i++) {
         var choices = questionsArray[questions].choices[i];
 
         var li = document.createElement("li");
         li.textContent = choices;
-        choicesListOl.appendChild(li);
+        choicesListul.appendChild(li);
 
     };
 
 };
 
 
-function appendOlEl(){
-    choicesListOl = document.createElement("ol");
-    choicesListOl.setAttribute("id", "choices-list");
-    document.getElementById("ol-container").appendChild(choicesListOl);
+function appendUlEl() {
+    choicesListul = document.createElement("<ul>");
+    choicesListul.setAttribute("id", "choices-list");
+    document.getElementById("ul-container").appendChild(choicesListUl);
 
 };
 
-function removeOlEl(){
+function removeUlEl() {
 
-    choicesListOl.remove();
+    choicesListUl.remove();
 };
 
-function answerCheck(userChooses){
-    if (userChooses !== answer){
-       timeLeft -= 10 
+function answerCheck(userChooses) {
+    if (userChooses !== answer) {
+        timeLeft -= 10
     }
 
     questions++;
 };
 
-function quizEnded(){
-    timeEl.textContent ="Quiz has Ended!";
-    questionsEl.textContent = "Your Score is" + userScore;
-    removeOlEl();
+function quizEnded() {
+    timeElement.textContent = "Quiz has Ended!";
+    questionsElement.textContent = "Your Score is" + userScore;
+    removeUlEl();
 
-    startBtn.style.display ="inline-block";
+    startButton.style.display = "inline-block";
 };
 
+startButton.addEventListener("click", startQuiz);
 
 
+uLContainerElement.addEventListener("click", function (event) {
+    answerCheck(event.target.trim());
+    removeOlEl();
+    theQuestions();
 
-
-startButton.onclick= function(event) {
-    event.startTimeQuiz();
-  };
-
-
-
+})
 
